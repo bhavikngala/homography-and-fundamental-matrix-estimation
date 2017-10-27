6b898d29f31dd31df49a77cd325cf60c8de6e594
Bhavik N Gala <bhavikna@buffalo.edu>
2017-10-27 17:40:25 -0400

    step 1 - feature detections (harris)

--- code/test.m
+++ code/test.m
@@ -1,7 +1,13 @@
 close all;
 
 % read images 
-I1 = rgb2gray(imdouble(imread('./../data/')));
-I2 = rgb2gray(imdouble(imread('./../data')));
+I1 = rgb2gray(imread('./../data/part1/uttower/left.jpg'));
+I2 = rgb2gray(imread('./../data/part1/uttower/right.jpg'));
 
 % step 1 - feature extraction
+% harris feature detection
+% TODO: harris is not scale invariant
+% try to implement SIFT for scale invariance
+[cim1, r1, c1] = harris(I1, 1.618, 2000, 3, 1);
+[cim2, r2, c2] = harris(I2, 1.618, 2000, 3, 1);
+
