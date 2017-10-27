39575f0eeffe4fa152bc207000d77aa0e3ad8617
Bhavik N Gala <bhavikna@buffalo.edu>
2017-10-27 18:27:06 -0400

    modified detectFeatures -> detectFeaturesHarris to return coordinates + neighboursizes

---
+++ code/detectFeaturesHarris.m
@@ -0,0 +1,16 @@
+function [rows, cols, neighbourhoodSize] = ...
+    detectFeaturesHarris(I, sigma, threshold, radius, displayFlag)
+%DETECTFEATURES Summary of this function goes here
+%   Detailed explanation goes here
+
+% function used to detect features on a image
+% TODO: should take type of features to detect as user input.
+% For now perform SURF feature detection and return [rows, cols]
+
+% ouput should return neighbourhood size along with keypoints
+    
+    [~, rows, cols] = harris(I, sigma, threshold, radius, displayFlag);
+    neighbourhoodSize = (2 * radius + 1) * ones([size(rows, 1), 1]);
+
+end
+
