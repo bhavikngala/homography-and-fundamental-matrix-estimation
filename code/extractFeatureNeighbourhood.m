3eda7c3ba507dc5b1ff0b558ca781da0691927b2
Bhavik N Gala <bhavikna@buffalo.edu>
2017-10-27 19:22:10 -0400

    bug fix - dimension of temp did not match numCOlumns in descriptors

--- code/extractFeatureNeighbourhood.m
+++ code/extractFeatureNeighbourhood.m
@@ -4,19 +4,19 @@ function [descriptors] = extractFeatureNeighbourhood(I, r, c, ...
 %   Detailed explanation goes here
 
     [rows, cols] = size(I);
-    descriptors = zeros([10, neighourhoodSize]);
+    descriptors = zeros([10, neighourhoodSize ^ 2]);
     descriptorRowIndex = 1;
     
     for i=1:1:size(r, 1)
-        xOffset = floor(c(i, 1) / 2);
-        yOffset = floor(r(i, 1) / 2);
-        sx = c(i, 1) - xOfffset;
-        sy = r(i, 1) - yOffset;
-        ex = c(i, 1) + xOffset;
-        ey = c(i, 1) + yOffset;
+        offset = floor(neighourhoodSize / 2);
+        sx = r(i, 1) - offset;
+        sy = c(i, 1) - offset;
+        ex = r(i, 1) + offset;
+        ey = c(i, 1) + offset;
         
         if 1 <= sy && rows >= ey && 1 <= sx && cols >= ex
             temp = I(sy:ey, sx:ex);
+            temp = temp';
             descriptors(descriptorRowIndex, :) = temp(:);
             descriptorRowIndex = descriptorRowIndex + 1;
         end;
