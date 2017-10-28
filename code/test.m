e26c3c3864fbae77042f75758a5fe9e669cdcba8
Bhavik N Gala <bhavikna@buffalo.edu>
2017-10-28 10:46:37 -0400

    step 3.a - compute euclidean distance between descriptors

--- code/test.m
+++ code/test.m
@@ -14,7 +14,7 @@ I2 = rgb2gray(imread('./../data/part1/uttower/right.jpg'));
 [r2, c2, nhbhs2] = detectFeaturesHarris(I2, 1.618, 2000, 3, 1);
 
 % step 2 - Extract local neighborhoods around every keypoint in both 
-% images, and form descriptors simply by flattening" the pixel values in 
+% images, and form descriptors simply by "flattening" the pixel values in 
 % each neighborhood to one-dimensional vectors. Experiment with different 
 % neighborhood sizes to see which one works the best. If you're using your 
 % Laplacian detector, use the detected feature scales to define the 
@@ -22,3 +22,16 @@ I2 = rgb2gray(imread('./../data/part1/uttower/right.jpg'));
 
 featureNeighbourhood1 = extractFeatureNeighbourhood(I1, r1, c1, nhbhs1);
 featureNeighbourhood2 = extractFeatureNeighbourhood(I2, r2, c2, nhbhs2);
+
+% step 2.b -  mean normalize the descriptors.
+meanNormalizedFeatureNeighbourhood1 = meanNormalize(featureNeighbourhood1);
+meanNormalizedFeatureNeighbourhood2 = meanNormalize(featureNeighbourhood2);
+
+% step 3.a - compute distance between descriptors - Euclidean distance
+euclid_dist = dist2(meanNormalizedFeatureNeighbourhood1, ...
+    meanNormalizedFeatureNeighbourhood2);
+
+% TODO: step 3.b - compute normalized correlation instead of euclidean
+% distance
+
+% step 4 - 
