0598a68000b1b2e9957030d72b325ed86e8407fd
Bhavik N Gala <bhavikna@buffalo.edu>
2017-10-30 00:32:51 -0400

    new method of getting putative matches and their subscripts

--- code/getPutativeMatches.m
+++ code/getPutativeMatches.m
@@ -2,26 +2,42 @@ function [r, c, nCorr] = getPutativeMatches(normCorr, howMany, ...
     thresholdPercent)
 %TOPFEWHUNDREDCORRELATEDDESCRIPTORS Summary of this function goes here
 %   Detailed explanation goes here
-    threshold = max(normCorr(:)) * thresholdPercent / 100;
+%     threshold = max(normCorr(:)) * thresholdPercent / 100;
     
-%     find values above threshold
-%     can improve performance with logical indicing
-    indicesOfValuesAboveOrEqualToThreshold = find(normCorr >= threshold);
+%     get column number which is highly correlated with the row
+    [values, maxCorrelatedColumns] = max(normCorr');
     
-%     sort valuesAboveThreshold based on values
-    [values, indicesOfValuesAboveOrEqualToThreshold] = sort( ...
-        normCorr(indicesOfValuesAboveOrEqualToThreshold), 'descend');
+    [values, indices] = sort(values, 'descend');
     
-%     return top howMany indices for the descriptors
-    if size(indicesOfValuesAboveOrEqualToThreshold, 1) >= howMany 
-        [r, c] = ind2sub(size(normCorr), ... 
-            indicesOfValuesAboveOrEqualToThreshold(1:howMany));
-        nCorr = values(1:howMany);
+    if size(normCorr, 1) >= howMany
+        r = indices(1:howMany);
     else
-        [r, c] = ind2sub(size(normCorr), ...
-            indicesOfValuesAboveOrEqualToThreshold);
-        nCorr = values;
-    end;
+        r = indices;
+    end; 
+    c = maxCorrelatedColumns(r);
+    
+%     r = r';
+%     c = c';
+    nCorr = values(1:howMany);
+    
+% %     find values above threshold
+% %     can improve performance with logical indicing
+%     indicesOfValuesAboveOrEqualToThreshold = find(normCorr >= threshold);
+%     
+% %     sort valuesAboveThreshold based on values
+%     [values, indicesOfValuesAboveOrEqualToThreshold] = sort( ...
+%         normCorr(indicesOfValuesAboveOrEqualToThreshold), 'descend');
+%     
+% %     return top howMany indices for the descriptors
+%     if size(indicesOfValuesAboveOrEqualToThreshold, 1) >= howMany 
+%         [r, c] = ind2sub(size(normCorr), ... 
+%             indicesOfValuesAboveOrEqualToThreshold(1:howMany));
+%         nCorr = values(1:howMany);
+%     else
+%         [r, c] = ind2sub(size(normCorr), ...
+%             indicesOfValuesAboveOrEqualToThreshold);
+%         nCorr = values;
+%     end;
 
 end
 
