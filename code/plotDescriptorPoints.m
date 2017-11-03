5dfbe0a909423286ed770f296fc5103b5fd8e381
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-03 18:40:50 -0400

    function plots marks descriptor coordinates on both the images rendered side by side

---
+++ code/plotDescriptorPoints.m
@@ -0,0 +1,23 @@
+function plotDescriptorPoints(I1, I2, r1, c1, r2, c2)
+%PLOTPUTATIVEMATCHES Summary of this function goes here
+%   Detailed explanation goes here
+    
+    I = [I1, I2];
+    figure;
+    imshow(I);
+    hold on;
+    
+    I1width = size(I1, 2);
+    
+    [r, c] = size(r1);
+    for i=1:1:r*c
+        plot(c1, r1, 'm*');
+    end;
+    
+    [r, c] = size(r2);
+    for i=1:1: r*c
+        plot(c2+I1width, r2, 'gx');
+    end;
+
+end
+
