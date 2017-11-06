601625b56d639785cd51e17b83f360512d8a4156
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-06 16:47:37 -0500

    function prototype/skeleton for ransac voting scheme

---
+++ code/ransacVoting.m
@@ -0,0 +1,8 @@
+function [numInliers, numOutliers, consensusXY, consensusXYPrime] = ...
+    ransacVoting(xy, xyprime, H, threshold)
+%COMPUTEINLIERSOUTLIERSFORMODEL Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
