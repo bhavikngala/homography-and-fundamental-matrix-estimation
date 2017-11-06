b60ef0ce01c161a900b11b40cf8b06fd75093be4
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-06 18:43:51 -0500

    function definition of ransac voting

--- code/ransacVoting.m
+++ code/ransacVoting.m
@@ -2,12 +2,28 @@ function [numInliers, numOutliers, consensusXY, consensusXYPrime] = ...
     ransacVoting(xy, xyprime, H, threshold)
 %COMPUTEINLIERSOUTLIERSFORMODEL Summary of this function goes here
 %   Detailed explanation goes here
+    numInliers = 0;
+    numOutliers = 0;
+    
+    consensusXY = zeros([4, 2]);
+    consensusXYPrime = zeros([4, 2]);
 
     for i=1:1:size(xy, 1)
+%         step 1 compute (x', y') from H and (x, y)
         xprimeyprime = H * [xy(i, :)'; 1];
-        xprimeyprime = xprimeyprime ./ xprimeyprime(3,1);
         xprimeyprime = bsxfun(@rdivide, xprimeyprime, xprimeyprime(3, 1));
         
+%         step 2 compute error
+        error = sum((xprimeyprime - xyprime(i, :)').^2);
+        
+%         step 3 threshold inlier or outlier
+        if error <= threshold
+            numInliers = numInliers + 1;
+            consensusXY(i, :) = xy(i, :);
+            consensusXYPrime(i, :) = xyprime(i, :);
+        else
+            numOutliers = numOutliers + 1;
+        end;
     end;
 
 end
