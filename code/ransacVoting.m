47a138fdac6a544ba95118e83f8c21a965bc3478
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-06 16:59:35 -0500

    compute xprime, yprime from homography matix

--- code/ransacVoting.m
+++ code/ransacVoting.m
@@ -3,6 +3,12 @@ function [numInliers, numOutliers, consensusXY, consensusXYPrime] = ...
 %COMPUTEINLIERSOUTLIERSFORMODEL Summary of this function goes here
 %   Detailed explanation goes here
 
+    for i=1:1:size(xy, 1)
+        xprimeyprime = H * [xy(i, :)'; 1];
+        xprimeyprime = xprimeyprime ./ xprimeyprime(3,1);
+        xprimeyprime = bsxfun(@rdivide, xprimeyprime, xprimeyprime(3, 1));
+        
+    end;
 
 end
 
