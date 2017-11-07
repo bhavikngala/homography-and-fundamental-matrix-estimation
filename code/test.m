7c4dc66b9d549fa86dc5609d7f9c4020d67f731f
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-06 19:35:10 -0500

    step 5 - compute H using ransac

--- code/test.m
+++ code/test.m
@@ -1,14 +1,4 @@
-% close all;
-
-addpath(genpath('D:\Computer Science\Sem1\CSE573-Intro  to Computer Vision and Image Processing\Homework 2\hw2\code'));
-imLeft = 'D:\Computer Science\Sem1\CSE573-Intro  to Computer Vision and Image Processing\Homework 3\data\part1\uttower\left.jpg';
-imRight = 'D:\Computer Science\Sem1\CSE573-Intro  to Computer Vision and Image Processing\Homework 3\data\part1\uttower\right.jpg';
-% parameters for blob detection
-sigma = 1.618; % 1.168
-k = 1.159; % 1.159
-layers = 15;
-thresholdPercent = 40; % this is percentage, should be in scale 0-100
-
+close all;
 % read images 
 I1 = im2double(rgb2gray(imread('./../data/part1/uttower/left.jpg')));
 I2 = im2double(rgb2gray(imread('./../data/part1/uttower/right.jpg')));
@@ -23,8 +13,6 @@ I2 = histeq(I2);
 % step 1 - Detect feature points in both images.
 [r1, c1, nhbhs1] = detectFeaturesHarris(I1, 1.618, 0.1, 5, 0);
 [r2, c2, nhbhs2] = detectFeaturesHarris(I2, 1.618, 0.1, 5, 0);
-fprintf('number of descriptors in I1 %d\n', size(r1, 1));
-fprintf('number of descriptors in I2 %d\n', size(r2, 1));
 
 % plotDescriptorPoints(I1, I2, r1, c1, r2, c2);
 
@@ -39,7 +27,17 @@ normCorr = computeNormalizedCorrelation(featureNeighbourhood1, ...
 
 % step 4 - select top few hundred of descriptors with 
 [topRow, topCol, topNormCorr] = getPutativeMatches(normCorr, 50, 95);
+topRow = topRow';
+topCol = topCol';
 
 % step 4.a - plot putative matches
 disp(size(topRow));disp(size(topCol));
-plotPutativeMatches(I1, I2, r1, c1, r2, c2, topRow', topCol');
+plotPutativeMatches(I1, I2, r1, c1, r2, c2, topRow, topCol);
+
+% step 5 - compute H using ransac
+ransacIterations = 1000;
+threshold = 0.1;
+
+[H, optimalNumInliers, optimalConsensusXY, optimalConsensusXYPrime, ...
+    optimalSampleSpace] = ransac([c1(topRow), r1(topRow)], ...
+    [c2(topCol), r2(topCol)], threshold, ransacIterations);
