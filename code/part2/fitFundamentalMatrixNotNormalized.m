31b85a5d1ae8c62c49600912d616fc94819faae5
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-08 16:02:15 -0500

    function definition - compute fundamental matrix - unnormalized equation

--- code/part2/fitFundamentalMatrixNotNormalized.m
+++ code/part2/fitFundamentalMatrixNotNormalized.m
@@ -1,7 +1,28 @@
 function [F] = fitFundamentalMatrixunnormalized(xy, xyprime)
 %FITFUNDAMENTALMATRIXUNNORMALIZED Summary of this function goes here
 %   Detailed explanation goes here
+    r = size(xy, 1);
+    A = zeros(r, 9);
     
-
+    expand = @(A) feval(@(x)x{:}, num2cell(A));
+    
+%     enforcing unit norm constraint
+%     construct matrix A, size rx9
+    for i = 1:1:r
+        [x, y] = expand(xy(i, :));
+        [xprime, yprime] = expand(xyprime(i, :));
+        
+        A(i, :) = [ x*xprime, x*yprime, x, y*xprime, y*yprime, y, ...
+                    xprime, yprime, 1];
+    end;
+    
+%     compute A.T * A
+    ATA = A' * A;
+%     compute SVD
+    [U, S, ~] = svd(ATA);
+%     find column number associated with min eigen value
+    [~, vectorColumn] = min(S(find(S)));
+%     form H from that column in U
+    F = reshape(U(:, vectorColumn), [3, 3])';
 end
 
