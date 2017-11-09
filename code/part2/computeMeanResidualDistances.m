eb5791b4740f5e1fdea3b1ce9beb9e12292b58d4
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-09 11:10:36 -0500

    function definition - compute mean residual distances

--- code/part2/computeMeanResidualDistances.m
+++ code/part2/computeMeanResidualDistances.m
@@ -3,6 +3,29 @@ function [meanResidualAlgebraicDistances, meanResidualGeometricDistances...
 %COMPUTEMEANRESIDUALDISTANCES Summary of this function goes here
 %   Detailed explanation goes here
 
+%     reference: lec16 epipolar geometry - slide 32
 
+    xy = [xy, ones(size(xy, 1))];
+    xyprime = [xyprime, ones(size(xyprime, 1))];
+      
+%     minimizing sum of squared algebraic distances between points xyprime
+%     and epipolar lines Fxy (or points xy and epipolar lines F'xyprime)
+    meanResidualAlgebraicDistances = avg((xyprime * F * xy').^2);
+    
+%     calculate epipolar lines
+    leftEpipoleLines = (F' * xyprime')';
+    rightEpipoleLines = (F * xy')';
+    
+%     compute |Am + Bn + c|
+    leftDotProduct = abs(dot(xy, leftEpipoleLines, 2));
+    rightDotProduct = abs(dot(xyprime, rightEpipoleLines, 2));
+%     compute sqrt(A^2 + B^2)
+    leftGDDeno = leftEpipoleLines(:, 1).^2 + leftEpipoleLines(:, 2).^2;
+    rightGDDeno = rightEpipoleLines(:, 1).^2 + rightEpipoleLines(:, 2).^2;
+%     compute geometric distance avg
+    meanResidualGeometricDistances = ...
+        avg(bsxfun(@rdivide, leftDotProduct, leftGDDeno) + ...
+            bsxfun(@rdivide, rightDotProduct, rightGDDeno));
+    
 end
 
