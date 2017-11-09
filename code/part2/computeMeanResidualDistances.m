9b9a002cd3f79a82464d70b7bdfb4779b318ff8a
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-09 10:18:41 -0500

    function prototype - computes mean residual algebraic and geometric distances

---
+++ code/part2/computeMeanResidualDistances.m
@@ -0,0 +1,8 @@
+function [meanResidualAlgebraicDistances, meanResidualGeometricDistances...
+    ] = computeMeanResidualDistances(xy, xyprime, F)
+%COMPUTEMEANRESIDUALDISTANCES Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
