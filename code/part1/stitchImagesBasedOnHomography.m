8b084b9eb8602506a3729c98b5022b4e3fe09540
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-09 17:02:03 -0500

    implementation for stitching colored images

--- code/part1/stitchImagesBasedOnHomography.m
+++ code/part1/stitchImagesBasedOnHomography.m
@@ -10,38 +10,27 @@ function [stitchedImage] = stitchImagesBasedOnHomography(I1, I2, H)
     
     T1 = maketform('projective', source(1:2, :)', dest(1:2, :)');
     [I1T, xdata, ydata] = imtransform(I1, T1, 'bicubic');
-%     figure; imshow(I1T);
-    
-    T2 = maketform('projective', dest(1:2, :)', source(1:2, :)');
-    [I2T, xdata, ydata] = imtransform(I2, T2, 'bicubic');
-%     figure; imshow(I2T);
     
 %     find the farthest point in the negative x direction
     nx = abs(ceil(min(dest(1, :))));
     ny = abs(ceil(min(dest(2, :))));
     
 %     find the farthest point in the positive y direction
-    px = abs(ceil(max(dest(1, :))));
-    py = abs(ceil(max(dest(2, :))));
+%     px = abs(ceil(max(dest(1, :))));
+%     py = abs(ceil(max(dest(2, :))));
     
-    [i2r, i2c] = size(I2);
+    [i2r, i2c, ~] = size(I2);
+
+    composite1 = zeros([ny + i2r + 1, nx + i2c + 1, 3]);
+    composite2 = zeros([ny + i2r + 1, nx + i2c + 1, 3]);
     
-    composite1 = zeros([ny + i2r + 1, nx + i2c + 1]);
-    composite2 = zeros([ny + i2r + 1, nx + i2c + 1]);
-    
-    [r, c] = size(I1T);
-    composite1(1:r, 1:c) = I1T;
-    composite2(ny + 2:ny + i2r + 1, nx + 2:nx + i2c + 1) = I2;
-    disp('composite 1')
-    size(composite1)
-    disp('composite 2')
-    size(composite2)
+    [r, c, ~] = size(I1T);
+    composite1(1:r, 1:c, :) = I1T;
+    composite2(ny + 2:ny + i2r + 1, nx + 2:nx + i2c + 1, :) = I2;
     
     stitchedImage = composite1 + composite2;
     overlap = composite1 & composite2;
     stitchedImage(overlap) = stitchedImage(overlap)/2;
-    
-    figure; imshow(stitchedImage);
 
 end
 
