80e060dafbdcd810b7df389d86aefbe250ee4554
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-10 17:31:00 -0500

    changed implementation to non-isotropic scaling

--- code/part2/computeCoordinateNormalizingMatrix.m
+++ code/part2/computeCoordinateNormalizingMatrix.m
@@ -4,6 +4,7 @@ function [T] = computeCoordinateNormalizingMatrix(xy)
 
 %     compute mean of x coordinates and y coordinates
     xymean = mean(xy);
+    principalmoments = max(xy);
 
 %     substract xmean from x and ymean from y
 %     compute distance from origin
@@ -14,8 +15,10 @@ function [T] = computeCoordinateNormalizingMatrix(xy)
     xyvar = var(xydist);
     
 %     construct the matrix
-    T = [1/xyvar 0       -1*xymean(1, 1);...
-         0       1/xyvar -1*xymean(1, 2);...
+    T = [1/principalmoments(1, 1), 0, ...
+        -1*xymean(1, 1)/principalmoments(1, 1);
+         0, 1/principalmoments(1, 2), ...
+         -1*xymean(1, 2)/principalmoments(1, 2);...
          0       0        1];
 
 end
