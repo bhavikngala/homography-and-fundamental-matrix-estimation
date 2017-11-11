d8a7358227b1f67a311f06b82b62bbd6c3b54001
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-11 02:06:19 -0500

    code refactoring

--- code/part2/computeCoordinateNormalizingMatrix.m
+++ code/part2/computeCoordinateNormalizingMatrix.m
@@ -2,23 +2,14 @@ function [T] = computeCoordinateNormalizingMatrix(xy)
 %COMPUTECOORDINATENORMALIZINGMATRIX Summary of this function goes here
 %   Detailed explanation goes here
 
+%     Non isotropic normalization
 %     compute mean of x coordinates and y coordinates
     xymean = mean(xy);
     principalmoments = max(xy);
-
-%     substract xmean from x and ymean from y
-%     compute distance from origin
-%     xydist = sqrt(sum((bsxfun(@minus, xy ,xymean)).^2, 2));
-    xydist = sqrt(sum(xy.^2, 2));
-
-%     compute variance of distances from origin
-    xyvar = var(xydist);
     
 %     construct the matrix
-    T = [1/principalmoments(1, 1), 0, ...
-        -1*xymean(1, 1)/principalmoments(1, 1);
-         0, 1/principalmoments(1, 2), ...
-         -1*xymean(1, 2)/principalmoments(1, 2);...
+    T = [1/principalmoments(1,1), 0, -1*xymean(1, 1)/principalmoments(1,1);
+         0, 1/principalmoments(1,2), -1*xymean(1, 2)/principalmoments(1,2);...
          0       0        1];
 
 end
